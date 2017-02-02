e1905c33d5115bc52fb07821bc4112c78d12a7b7
Ronald T. Fernandez <ronaldtfernandez@gmail.com>
2017-02-02 22:37:44 +0100

    Updated cxx makefile

--- cxx/cxx.mk
+++ cxx/cxx.mk
@@ -7,3 +7,5 @@ all:
 	g++ $(FLAGS) $(EXTRA_INC) -o db/DBConnectionPool.o -c db/DBConnectionPool.cpp
 	g++ $(FLAGS) $(EXTRA_INC) -o db/DBConnector.o -c db/DBConnector.cpp
 	g++ $(FLAGS) $(LDFLAGS) $(CPPFLAGS) $(EXTRA_INC) $(EXTRA_LIB) -o rest/Server db/DBConnectionPool.o db/DBConnector.o utils/Utils.o rest/Server.cpp
+clean:
+	rm -rf rest/Server */*.o */*/*.o
