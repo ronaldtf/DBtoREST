e921c9d83fac228bfb28ccf1429643fd54516ecc
Ronald T. Fernandez <ronaldtfernandez@gmail.com>
2017-03-06 21:03:38 +0100

    Added Json library and prepare REST Handler

--- java/src/main/java/rest/RESTHandler.java
+++ java/src/main/java/rest/RESTHandler.java
@@ -2,12 +2,16 @@ package main.java.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import main.java.db.connection.DBExecutor;
+import main.java.db.table.Column;
 
 @Path("/")
 public class RESTHandler {
@@ -16,21 +20,36 @@ public class RESTHandler {
 	@Path("/dbs")
 	@Produces(MediaType.APPLICATION_JSON)
 	public static String getDBs() {
-		return "dbs";
+		try {
+			Vector<String> dbs = DBExecutor.getDBs();
+			return "dbs";
+		} catch (Exception e) {
+			return "";
+		}
 	}
 	
 	@GET
 	@Path("/alltables/{dbName}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public static String getTables(@PathParam("dbName") String dbName) {
-		return dbName;
+		try {
+			Vector<String> dbs = DBExecutor.getTables(dbName);
+			return "dbs";
+		} catch (Exception e) {
+			return "";
+		}
 	}
 	
 	@GET
 	@Path("/table/{dbName}/{tableName}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public static String getTableInfo(@PathParam("dbName") String dbName, @PathParam("tableName") String tableName) {
-		return dbName + " " + tableName;
+		try {
+			Column table = DBExecutor.getTableInfo(dbName, tableName);
+			return "dbs";
+		} catch (Exception e) {
+			return "";
+		}
 	}
 	
 }
